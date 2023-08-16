f533a9ecc9fd30dd55995d8db98a4cf914b8aacb
icotwmfro <142396500+icotwmfro@users.noreply.github.com>
2023-08-16 12:56:52 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-(function (){
-    const burger = document.querySelector('.burger');
-	$('.offcanvas').on('hidden.bs.offcanvas', function () {
-	    burger.classList.toggle('burger--active');
-	});
-	$('.offcanvas').on('shown.bs.offcanvas', function () {
-	    burger.classList.toggle('burger--active');
-	});
-}())
